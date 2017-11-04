86768e0383385eec1ae1ccadb4e377aacbc917c9
Wael Al-Atrash <wassabi.dietaube@gmail.com>
2017-11-04 11:50:22 +0100

    computer grahic shit

---
+++ application/include/application_solar.hpp
@@ -0,0 +1,40 @@
+#ifndef APPLICATION_SOLAR_HPP
+#define APPLICATION_SOLAR_HPP
+
+#include "application.hpp"
+#include "model.hpp"
+#include "structs.hpp"
+#include "planet.h"
+
+// gpu representation of model
+class ApplicationSolar : public Application {
+ public:
+  // allocate and initialize objects
+  ApplicationSolar(std::string const& resource_path);
+  // free allocated objects
+  ~ApplicationSolar();
+
+  // update uniform locations and values
+  void uploadUniforms();
+  // update projection matrix
+  void updateProjection();
+  // react to key input
+  void keyCallback(int key, int scancode, int action, int mods);
+  //handle delta mouse movement input
+  void mouseCallback(double pos_x, double pos_y);
+
+  // draw all objects
+  void render() const;
+  void Planet1(Planet) const;
+
+
+ protected:
+  void initializeShaderPrograms();
+  void initializeGeometry();
+  void updateView();
+
+  // cpu representation of model
+  model_object planet_object;
+};
+
+#endif
---
+++ application/include/planet.h
@@ -0,0 +1,14 @@
+struct Planet {
+	float scaleX;
+	float scaleY;
+	float scaleZ;
+
+	float translationX;
+	float translationY;
+	float translationz;
+
+	float timeDiff;
+
+};
+
+	
